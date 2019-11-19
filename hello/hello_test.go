e8c722efb7a8e60eb8dc2bf67bdfa91999ef6f37
nagamocha3000 <nagamocha@gmail.com>
2019-11-19 08:31:55 +0300

    add language parameter to hello func

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,10 +3,38 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
-
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		//tells testing suite that this method is a helper
+		// when test fails, the number line reported will be where this
+		// fn is called rather than at the t.Errorf(...) line
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris", "")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("saying hello when empty string as arg", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in french", func(t *testing.T) {
+		got := Hello("Marie", "French")
+		want := "Bonjour, Marie"
+		assertCorrectMessage(t, got, want)
+	})
+
 }
