f1406665a99d47bf9166004d536d981a6c2ec322
nagamocha3000 <nagamocha@gmail.com>
2019-11-19 09:21:58 +0300

    complete iteration chapt

---
+++ iteration/repeat.go
@@ -0,0 +1,8 @@
+package iteration
+
+import "strings"
+
+//Repeat takes a string and repeats it n times returning the result
+func Repeat(str string, n int) string {
+	return strings.Repeat(str, n)
+}
